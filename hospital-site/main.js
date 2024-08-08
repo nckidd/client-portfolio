38f54ba161e107bb7ad8fbfae5d88a9efdb80370
nckidd <christenekidd@gmail.com>
2024-08-07 19:24:43 -0700

    add facilities and review pages

--- hospital-site/main.js
+++ hospital-site/main.js
@@ -5,7 +5,22 @@ $(document).ready(function() {
     });
 
     $(window).on('load scroll', function(){
-        $('fa-bars').removeClass('fa-times');
+        $('.fa-bars').removeClass('fa-times');
         $('.nav').removeClass('nav-toggle');
+
+        if($(window).scrollTop() > 10) {
+            $('header').addClass('header-active');
+        } else {
+            $('header').removeClass('header-active');
+        }
     });
+
+    $('.facility').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true,
+        }
+    });
+
 });
